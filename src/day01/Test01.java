c2b27da2a9446466d647ca7f564771ba52d256d3
ybh <yoobh96@daum.net>
2022-05-13 20:31:30 +0900

    day01

--- src/day01/Test01.java
+++ src/day01/Test01.java
@@ -1,14 +1,34 @@
 package day01;
 
 public class Test01 {
-	public static void main(String [] args) {
-
-		System.out.println("학원에서 작업했습니다!");
-		//학원에서 작업했슴다!
-
-		System.out.println("Hello World!");
-		//집에서 작업했슴다!
-
+	public static void main(String[] args) {
+		
+		System.out.println("안녕하세요~~");
+		System.out.println("안녕히가세요@@");
+		
+		
+		System.out.print("안녕하세요~~");
+		System.out.println("안녕히가세요@@");
+		
+		System.out.println();
+		int a=5;
+		System.out.printf("%d입니다",a);
+		
+		
+		System.out.println("이름 : 유병현");
+		System.out.println("나이 : 25살");
+		System.out.println("주소 : 서울시 강동구 암사동");
+		
+		
+		System.out.println("\\");
+		System.out.println("\"\\\\http 안 녕\"");
+		
+		
+		System.out.println( 100 );
+		System.out.println( 100.123 );
+		 		
+		
+		
 	}
 }
 
---
+++ src/day01/test02.java
@@ -0,0 +1,34 @@
+package day01;
+
+public class test02 {
+
+	public static void main(String[] args) {
+		
+		String name="유병현";
+		int age=25;
+		double height=183.0;
+		int iq=100;
+		char rank='A';
+		
+		System.out.println("이름 :"+name);
+		System.out.println("나이 :"+age);
+		System.out.println("키 :"+height);
+		System.out.println("아이큐 :"+iq);
+		System.out.println("등급 :"+rank);
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
